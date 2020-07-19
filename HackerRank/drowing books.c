bb1da1637964e1223dce0fce1f2de85c21d1aca2
Mayank9568 <46962063+Mayank9568@users.noreply.github.com>
2020-07-19 16:32:01 +0530

    Update drowing books.c

--- HackerRank/drowing books.c
+++ HackerRank/drowing books.c
@@ -9,26 +9,7 @@ int main()
 {
     int n,p,count=0;
     cin>>n>>p;
-
-    if(p<=n/2){
-
-
-    for(int i=0;i<p;i++){
-     count++;
-    }
-
-    }
-     if(p>n/2){
-
-
-    for(int i=n;i>p;i--){
-     count++;
-    }
-
-    }
-    // if(n%2!=0){
-    //     count++;
-    //     }
-    cout<<count/2;
+     count=min(p/2,n/2-p/2);
+    cout<<count;
     return 0;
 }
