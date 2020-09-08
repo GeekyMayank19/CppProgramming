c700eca57785e9e3a63fc35fcd2b1f7659ddbdd1
Mayank Sharma <46962063+GeekyMayank19@users.noreply.github.com>
2020-09-08 11:37:48 +0530

    Create Sherlock and Squares.cpp

---
+++ HackerRank/Sherlock and Squares.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+//https://www.hackerrank.com/challenges/sherlock-and-squares/problem
+using namespace std;
+
+typedef long long LL;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        long long int a, b, ans = 0, i;
+        cin >> a >> b;
+        for(i=1; ; i++) {
+            if (i * i > b) {
+                break;
+            }
+            if (i * i <= b && i * i >= a){
+                ans++;
+            }
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
