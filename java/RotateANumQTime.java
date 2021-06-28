8ee5dc645f6ab3a5cf93385df313f31696702638
Mayank Sharma <46962063+GeekyMayank19@users.noreply.github.com>
2021-06-28 21:02:04 +0530

    Create RotateANumQTime.java

---
+++ java/RotateANumQTime.java
@@ -0,0 +1,35 @@
+/**
+ * RotateANumQTime
+ */
+public class RotateANumQTime {
+
+    public static void main(String[] args) {
+        // write your code here  
+        Scanner scn = new Scanner(System.in);
+        int n= scn.nextInt();
+        int k = scn.nextInt();
+       //find no of digit 
+        int num=n;
+        int cnt=0;
+        while(num!=0){
+            num=num/10;
+            cnt++;
+        }
+       
+       //bring range of k
+       k=k%cnt;
+       if(k<0){
+           k=cnt+k;
+       }   
+        // divisor and multiple
+        int div = (int)Math.pow(10,k);
+        int div1 = (int)Math.pow(10,cnt-k);
+        int q= n/div;
+        int r= n%div;
+        //make answer
+        r=r*div1+q;
+        
+        System.out.println(r);
+        
+       }
+}
